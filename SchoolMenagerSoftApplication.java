d00760cd933f4cc136fdeca02e00ee7498d5e02f
Kruchyxe <ragamuffin@o2.pl>
2021-04-10 14:26:17 +0200

    first commit

---
+++ SchoolMenagerSoftApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.schoolmenagersoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMenagerSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolMenagerSoftApplication.class, args);
+    }
+
+}
