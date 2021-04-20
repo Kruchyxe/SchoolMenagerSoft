e2040fb060ed807fe393cddce52289b4a3d184a2
Kruchyxe <ragamuffin@o2.pl>
2021-04-20 10:23:28 +0200

    test of findAll method StudentServiceImplTest

--- test/java/pl/coderslab/schoolmenagersoft/service/StudentServiceImplTest.java
+++ test/java/pl/coderslab/schoolmenagersoft/service/StudentServiceImplTest.java
@@ -1,28 +1,57 @@
 package pl.coderslab.schoolmenagersoft.service;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.coderslab.schoolmenagersoft.repository.StudentRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class StudentServiceImplTest {
 
-    @Test
-    void addStudent() {
+    @Mock
+    private StudentRepository studentRepository;
+    private StudentServiceImpl underTest;
+
+    @BeforeEach
+    void setUp(){
+        underTest = new StudentServiceImpl(studentRepository);
+
     }
 
     @Test
-    void findAllStudents() {
+    @Disabled
+    void shouldAddStudent() {
+
     }
 
     @Test
-    void get() {
+    void shouldFindAllStudents() {
+        // when
+        underTest.findAllStudents();
+
+        // then
+        verify(studentRepository).findAll();
+
     }
 
     @Test
-    void deleteStudentById() {
+    @Disabled
+    void shouldGetStudent() {
     }
 
     @Test
-    void updateStudent() {
+    @Disabled
+    void shouldDeleteStudentById() {
+    }
+
+    @Test
+    @Disabled
+    void shouldUpdateStudent() {
     }
 }
