58c7b13edf0d46d2625d138f6a67ef368b2c9406
Kruchyxe <ragamuffin@o2.pl>
2021-04-21 19:01:20 +0200

    adding new methods to InventoryController and adding new views

--- main/java/pl/coderslab/schoolmenagersoft/controller/InventoryController.java
+++ main/java/pl/coderslab/schoolmenagersoft/controller/InventoryController.java
@@ -2,9 +2,17 @@ package pl.coderslab.schoolmenagersoft.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.schoolmenagersoft.service.InventoryService;
+import pl.coderslab.schoolmenagersoft.web.dto.GroupDto;
+import pl.coderslab.schoolmenagersoft.web.dto.InventoryDto;
+
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/inventories")
@@ -17,8 +25,35 @@ public class InventoryController {
     }
 
     @GetMapping()
-    public String employeesList(Model model){
+    public String inventoriesList(Model model){
         model.addAttribute("inventory", inventoryService.findAllInventories());
         return "inventories";
+    }
+
+    @GetMapping("/addinventory")
+    public String showAddGroupForm(Model model){
+        model.addAttribute("inventory", new InventoryDto());
+        return "addinventory";
+    }
+
+    @PostMapping("/addinventory")
+    public String addNewEmployee(@Valid @ModelAttribute("inventory") InventoryDto inventoryDto , BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "addinventory";
+        }
+        inventoryService.addInventory(inventoryDto);
+        return "redirect:/groups";
+    }
+
+    @GetMapping("/detailsinventory")
+    public String showDetailForm(long id, Model model) {
+        model.addAttribute("inventory", inventoryService.getInventory(id).orElseThrow(EntityNotFoundException::new));
+        return "detailsinventory";
+    }
+
+    @GetMapping("/deleteinventory")
+    public String deleteGroup(Long id){
+        inventoryService.deleteInventoryById(id);
+        return "redirect:/inventories";
     }
 }
